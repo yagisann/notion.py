bc9755b22820dac9aea4f14bd7785236a1c01564
yagi <121270079+yagisann@users.noreply.github.com>
2023-09-15 14:23:48 +0900

    temp update

--- notion/page_property.py
+++ notion/page_property.py
@@ -4,16 +4,21 @@ This model can be used in GET page requests
 
 https://developers.notion.com/reference/property-value-object
 """
+from __future__ import annotations
 from notion.base_model import Connect
 from pydantic import EmailStr, HttpUrl
+from .exceptions import ClientMissingError
 from .base_model import NotionObjectModel, NotionBaseModel
-from .base_object import DateObject, SelectOption, SelectOptionList, OptionGroup, StatusOptions
+from .general_object import DateObject, PartialSelectOption, SelectOption, SelectOptionList, OptionGroup, StatusOptions
 from .user import User
 from .file import File
-from .rich_text import RichText as RichTextPayload
-from typing import Literal, Union
+from .rich_text import Text, RichText as RichTextUnion
+from .notion_client.helpers import query_finder
+from typing import Literal, Union, TYPE_CHECKING
 from datetime import datetime as dt
 
+if TYPE_CHECKING:
+    from .page import Page
 
 class BoolFormulaValues(NotionBaseModel):
     type: Literal["boolean"]
@@ -45,9 +50,11 @@ FormulaValues = Union[
 
 class BasePageProperty(NotionBaseModel):
     id: str
+    editable: bool = True
 
-    def __init__(self, connect: Connect = None, **kwargs) -> None:
+    def __init__(self, connect: Connect = None, parent: Page = None, **kwargs) -> None:
         super().__init__(connect, **kwargs)
+        self.parent = parent
         self.belong_to = None
         self.find_column()
 
@@ -66,24 +73,28 @@ class CreatedBy(BasePageProperty):
     """ uneditable """
     type: Literal["created_by"]
     created_by: User
+    editable: bool = False
 
 
 class CreatedTime(BasePageProperty):
     """ uneditable """
     type: Literal["created_time"]
     created_time: dt
+    editable: bool = False
 
 
 class LastEditedBy(BasePageProperty):
     """ uneditable """
     type: Literal["last_edited_by"]
     last_edited_by: User
+    editable: bool = False
 
 
 class LastEditedTime(BasePageProperty):
     """ uneditable """
     type: Literal["last_edited_time"]
     last_edited_time: dt
+    editable: bool = False
 
 
 class MultiSelect(BasePageProperty):
@@ -92,6 +103,25 @@ class MultiSelect(BasePageProperty):
 
     def get_value(self):
         return [i.name for i in super().get_value()]
+    
+    def validate(self, options):
+        if self.belong_to:
+            col_options = self.belong_to.multi_select.options
+            for op in options:
+                if op not in col_options:
+                    raise ValueError("invalid option name for this property. valid option names: "+str([i.name for i in col_options]))
+    
+    def set_options(self, options: list[str]):
+        formed_options = []
+        for op in options:
+            if isinstance(op, PartialSelectOption):
+                formed_options.append(op)
+            elif isinstance(op, str):
+                formed_options.append(PartialSelectOption(name=op))
+            else:
+                raise TypeError("options should be list of str, ParialSelectOption, SelectOption.")
+        self.validate(formed_options)
+        self.multi_select = formed_options
 
 
 class Select(BasePageProperty):
@@ -102,6 +132,22 @@ class Select(BasePageProperty):
         val = super().get_value()
         return None if val is None else val.name
 
+    def validate(self, option):
+        if self.belong_to:
+            col_options = self.belong_to.select.options
+            if option not in col_options:
+                raise ValueError("invalid option name for this property. valid option names: "+str([i.name for i in col_options]))
+    
+    def set_option(self, option: str):
+        if isinstance(option, PartialSelectOption):
+            formed_option = option
+        elif isinstance(option, str):
+            formed_option = PartialSelectOption(name=option)
+        else:
+            raise TypeError("options should be list of str, ParialSelectOption, SelectOption.")
+        self.validate(formed_option)
+        self.select = formed_option
+
 
 class Status(BasePageProperty):
     type: Literal["status"]
@@ -111,21 +157,53 @@ class Status(BasePageProperty):
         val = super().get_value()
         return None if val is None else val.name
 
+    def validate(self, option):
+        if self.belong_to:
+            col_options = self.belong_to.status.options
+            if option not in col_options:
+                raise ValueError("invalid option name for this property. valid option names: "+str([i.name for i in col_options]))
+    
+    def set_status(self, option: str):
+        if isinstance(option, PartialSelectOption):
+            formed_option = option
+        elif isinstance(option, str):
+            formed_option = PartialSelectOption(name=option)
+        else:
+            raise TypeError("options should be list of str, ParialSelectOption, SelectOption.")
+        self.validate(formed_option)
+        self.status = formed_option
+
 
 class Title(BasePageProperty):
     type: Literal["title"]
-    title: list[RichTextPayload]
+    title: list[RichTextUnion]
 
     def get_value(self):
         return "".join([i.plain_text for i in super().get_value()])
+    
+    def set_text(self, text: str):
+        if isinstance(text, str):
+            self.title = Text.new(text)
+        elif isinstance(text, Text):
+            self.title = text
+        else:
+            raise TypeError("text should be one of str, Text")
 
 
 class RichText(BasePageProperty):
     type: Literal["rich_text"]
-    rich_text: list[RichTextPayload]
+    rich_text: list[RichTextUnion]
 
     def get_value(self):
         return "".join([i.plain_text for i in super().get_value()])
+
+    def set_text(self, text: str):
+        if isinstance(text, str):
+            self.title = Text.new(text)
+        elif isinstance(text, RichTextUnion):
+            self.title = text
+        else:
+            raise TypeError("text should be one of str, Text, MentionText, EquationText.")
 
 
 class Relation(BasePageProperty):
@@ -133,6 +211,21 @@ class Relation(BasePageProperty):
     has_more: bool
     # page relation object that contain id of page
     relation: list[NotionObjectModel]
+
+    async def get_pagenated_items(self):
+        if not self.has_more:
+            return
+        if client:=self._connect.client is None:
+            raise ClientMissingError()
+        self.relation = []
+        start_cursor = {}
+        has_more = 1
+        while has_more:
+            r = await client.pages.properties.retrieve(page_id=self.parent.id, property_id=self.id, **start_cursor)
+            for i in r["results"]:
+                self.relation.append(NotionObjectModel(**i["relation"]))
+            has_more = r["has_more"]
+            start_cursor = query_finder(r["next_cursor"])
 
 
 class Rollup(BasePageProperty):
